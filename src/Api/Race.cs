d48ce6cea60203165405b2911df7dd25734114d6
Maarten Kools <m.kools@rdc.nl>
2021-05-20 23:15:12 +0200

    Fixed the winning lap registration

--- src/Api/Race.cs
+++ src/Api/Race.cs
@@ -1,7 +1,6 @@
 ﻿using Api.Abstractions;
 using Api.Model;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Api
@@ -9,7 +8,6 @@ namespace Api
     public class Race : IRace
     {
         private readonly ILaptimeFeed laptimeFeed;
-        private readonly Dictionary<uint, uint> lapCount = new();
 
         public Race(ILaptimeFeed laptimeFeed)
         {
@@ -22,22 +20,17 @@ namespace Api
 
             await foreach (var laptime in this.laptimeFeed.ReadLaptimesAsync().ConfigureAwait(false))
             {
-                if (!this.lapCount.TryGetValue(laptime.Number, out var currentLap)) currentLap = 0;
-                currentLap++;
-
                 if (fastestLaptime == null)
                 {
                     fastestLaptime = laptime;
                 }
-                else if (laptime.Time > fastestLaptime.Time)
+                else if (laptime.Time < fastestLaptime.Time)
                 {
                     fastestLaptime = laptime;
                 }
 
                 // First kart to complete all laps finishes the race
-                if (currentLap == totalLaps) return fastestLaptime;
-
-                this.lapCount[laptime.Number] = currentLap;
+                if (laptime.Lap == totalLaps) return fastestLaptime;
             }
 
             throw new InvalidOperationException("The laptime feed doesn't provide enough laptimes");
